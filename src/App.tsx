4f5af3daa9d0471b8ac4c01e3172e342d2b712f5
christianAlvim <christianfelipialvim@gmail.com>
2024-05-17 08:14:05 -0300

    primeira vez no react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Tarefa from './pages/tarefa/Tarefa'
+
+function App() {
+
+  return (
+    <>
+        {/* <Home
+        titulo = 'Componente'
+        texto = 'Este e um componente que recebe props.' />
+        <Contador/> */}
+        <Tarefa/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,22 @@
+﻿import { useState } from "react";
+
+function Contador() {
+
+  const [valor, setvalor] = useState(0);
+
+  function handleClick(){
+    setvalor(valor + 1);
+  }
+
+  return (
+
+    <div>
+      <h2>Componente Contador</h2>
+      <p>O valor atural é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+      </div>
+  )
+}
+
+export default Contador
+
